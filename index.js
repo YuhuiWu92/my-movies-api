7c80cfc4826643541ef75a05b1938f553c9ca5ed
YuhuiWu92 <94534171+YuhuiWu92@users.noreply.github.com>
2022-02-12 01:22:40 +0100

    update all endpoints

--- index.js
+++ index.js
@@ -74,13 +74,123 @@ app.get("/movies/:Title", (req, res) => {
             res.status(500).send("Error" + err);
         });
 });
+// Return data about a genre (description) by name(e.g., “Action”)
+app.get("/genre/:Name", (req, res) => {
+    Movies.findOne({ "Genre.Name": req.params.Name })
+        .then((movie) => {
+            res.status(200).json(movie.Genre.Description);
+        })
+        .catch((err) => {
+            console.error(err);
+            res.status(500).send("Error" + err);
+        });
+});
+// Return data about a director (bio, birth year, death year) by name
+app.get("/director/:Name", (req, res) => {
+    Movies.findOne({ "Director.Name": req.params.Name })
+        .then((movie) => {
+            res.status(200).json(movie.Director);
+        })
+        .catch((err) => {
+            console.error(err);
+            res.status(500).send("Error" + err);
+        });
+});
+// Allow new users to register ==> add/post a user
+app.post('/users', (req, res) => {
+    Users.findOne({ Username: req.body.Username })
+        .then((user) => {
+            if (user) {
+                return res.status(400).send(req.body.Username + 'already exists');
+            } else {
+                Users
+                    .create({
+                        Username: req.body.Username,
+                        Password: req.body.Password,
+                        Email: req.body.Email,
+                        Birthday: req.body.Birthday
+                    })
+                    .then((user) => { res.status(201).json(user) })
+                    .catch((error) => {
+                        console.error(error);
+                        res.status(500).send('Error: ' + error);
+                    })
+            }
+        })
+        .catch((error) => {
+            console.error(error);
+            res.status(500).send('Error: ' + error);
+        });
+});
+
+// Allow users to update their user info (username, password, email, date of birth) ==> put 
+app.put('/users/:Username', (req, res) => {
+    Users.findOneAndUpdate({ Username: req.params.Username }, {
+            $set: {
+                Username: req.body.Username,
+                Password: req.body.Password,
+                Email: req.body.Email,
+                Birthday: req.body.Birthday
+            }
+        }, { new: true }, // This line makes sure that the updated document is returned
+        (err, updatedUser) => {
+            if (err) {
+                console.error(err);
+                res.status(500).send('Error: ' + err);
+            } else {
+                res.json(updatedUser);
+            }
+        });
+});
+
+// Allow users to add a movie to their list of favorites ==> post $push or $addToSet
+app.post('/users/:Username/movies/:MovieID', (req, res) => {
+    Users.findOneAndUpdate({ Username: req.params.Username }, {
+            $push: { FavoriteMovies: req.params.MovieID }
+        }, { new: true },
+        (err, updatedUser) => {
+            if (err) {
+                console.error(err);
+                res.status(500).send('Error: ' + err);
+            } else {
+                res.json(updatedUser);
+            }
+        });
+});
+
+// Allow users to remove a movie from their list of favorites ==> deleteOne
+
+app.delete('/users/:Username/movies/:MovieID', (req, res) => {
+    Users.findOneAndUpdate({ Username: req.params.Username }, {
+            $pull: { FavoriteMovies: req.params.MovieID }
+        }, { new: true },
+        (err, updatedUser) => {
+            if (err) {
+                console.error(err);
+                res.status(500).send('Error: ' + err);
+            } else {
+                res.json(updatedUser);
+            }
+        });
+});
+
+// Allow existing users to deregister
+app.delete('/users/:Username', (req, res) => {
+    Users.findOneAndRemove({ Username: req.params.Username })
+        .then((user) => {
+            if (!user) {
+                res.status(400).send(req.params.Username + ' was not found');
+            } else {
+                res.status(200).send(req.params.Username + ' was deleted.');
+            }
+        })
+        .catch((err) => {
+            console.error(err);
+            res.status(500).send('Error: ' + err);
+        });
+});
 
 // errorHandling
-// should I add this? I mean should I new and throw an Error first? or I do not need to do that,because the error-handling middleware function should log "all" application-level errors,so that if I throw only on of them,it will not enought?
-
-/* app.get('/', (req, res) => {
-    throw new Error('Something is wrong!')
-}) */
 
 app.use((err, req, res, next) => {
     console.error(err.stack);
