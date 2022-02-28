94256ac91982b1d8f030c2ef1ae39474afb7c522
Yuhui Wu <94534171+YuhuiWu92@users.noreply.github.com>
2022-02-28 19:46:35 +0100

    allow “localhost:1234” to connect to the app

--- index.js
+++ index.js
@@ -26,7 +26,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 // use CORS
 const cors = require('cors');
-let allowedOrigins = ['http://localhost:8080', 'http://te.com'];
+let allowedOrigins = ['http://localhost:8080', http://localhost:1234];
 app.use(cors({
     origin: (origin, callback) => {
         if (!origin) return callback(null, true);
