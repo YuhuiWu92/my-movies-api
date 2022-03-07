07b905446c2441bf4bc347a311e4d0948ea6cd51
YuhuiWu92 <94534171+YuhuiWu92@users.noreply.github.com>
2022-03-07 09:51:41 +0100

    allowed all origins cors

--- index.js
+++ index.js
@@ -26,17 +26,18 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 // use CORS
 const cors = require('cors');
-let allowedOrigins = ['http://localhost:8080', 'http://te.com'];
-app.use(cors({
-    origin: (origin, callback) => {
-        if (!origin) return callback(null, true);
-        if (allowedOrigins.indexOf(origin) === -1) { // If a specific origin isn’t found on the list of allowed origins
-            let message = 'The CORS policy for this application doesn’t allow access from origin ' + origin;
-            return callback(new Error(message), false);
-        }
-        return callback(null, true);
-    }
-}));
+// let allowedOrigins = ['http://localhost:8080', 'http://te.com'];
+// app.use(cors({
+//     origin: (origin, callback) => {
+//         if (!origin) return callback(null, true);
+//         if (allowedOrigins.indexOf(origin) === -1) { // If a specific origin isn’t found on the list of allowed origins
+//             let message = 'The CORS policy for this application doesn’t allow access from origin ' + origin;
+//             return callback(new Error(message), false);
+//         }
+//         return callback(null, true);
+//     }
+// }));
+app.use(cors{}); // Here we are allowing all the origins
 
 let auth = require('./auth')(app);
 const passport = require('passport');
